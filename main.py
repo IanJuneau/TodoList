3639e396194cd9028502d7f4a62069f9c73a7652
IanJuneau <juneau.ian@gmail.com>
2023-09-21 23:34:10 -0700

    Commands
    Adds in basic command functionality.
    Enchanced the List output
    Added some FIXME's

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import time
 import os
 import getpass
-
+import sys
 
 
 
@@ -17,18 +17,40 @@ username = getpass.getuser()
 
 #Function list
 def display():
+
     os.system('clear')
     #Headers
-    print(f"{'TaskName: ':<20}",end = ' ')
-    print(f"{'| Date: ':<8}")
+    top_bottom_border = '*'*50
+    print(top_bottom_border)
+    print(f"* {'TaskName: ':<20}",end = ' ')
+    print(f"{'| Date: ':<8}",end = '')
+    print(f"{' '*18}*")
+    #FIXME:Print ToDoList here
 
+    print(top_bottom_border)
+    print('\n\n')
 
+#FIXME: Add commands
 def handleInput(command):
-    print('yeah')
+    
+    if command == 'help' or command == 'h':
+        print('This program can handle a varity of commands')
+        print('Type \'(q)uit\' to exit the program')
+        print('Type \'(c)lear\ to clear the text and update the table')
+        print('Type \'(a)dd\ to schedule a new event')
+        print('Type \'(d)one\ to complete and remove a task from the list')
+        return
+    elif command == 'clear' or command == 'c':
+        display()
+    elif command == 'add' or command == 'a':
+        print('test<5j><5k>')
+    elif command == 'quit' or command == 'q':
+        sys.exit()
+
 
 def get_Input():
-    user_Input = input()
-    return user_Input
+    user_Input = input('Command: ')
+    return user_Input.lower()
 
 
 
@@ -45,7 +67,6 @@ def main():
     display()
     #Initial User instructions. I picked this so that new users can get help,
     #but old users won't always see a tutorial
-    print('\n\n\n')
     print(f'Welcome, {username}! Enjoy the program and type "help" if needed')
     
     #Main program loop
