9b59902ebc0704d9c970ee6522824e4e0f6c81da
IanJuneau <juneau.ian@gmail.com>
2023-09-21 21:58:50 -0700

    Skeleton
    This gets the skeleton outline for the program. It is able to create a data file if it is not there yet for the first time being run.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TodoList
+This is my first ever github project. I am currently a student studying aerospace engineering.
+I am taking a pytyhon class, so I am doing a quick python project for a todo list in the command line.
+I got this idea from talking with ChatGPT and think that it will be fun.
+In addition to practicing my python I will be attemping to learn about the git workflow. If you have any comments or tips please let me know!
--- main.py
+++ main.py
@@ -1,1 +1,63 @@
-print('Hello, World!')
+from pathlib import Path
+import time
+import os
+import getpass
+
+
+
+
+
+###############################Initial Variables####################################
+#Path for savefile
+data_Path = Path.cwd() / Path('data.txt')
+#Getting username
+username = getpass.getuser()
+#Initializing command variable
+
+
+#Function list
+def display():
+    os.system('clear')
+    #Headers
+    print(f"{'TaskName: ':<20}",end = ' ')
+    print(f"{'| Date: ':<8}")
+
+
+def handleInput(command):
+    print('yeah')
+
+def get_Input():
+    user_Input = input()
+    return user_Input
+
+
+
+
+
+
+def main():
+    command = '' 
+    if (data_Path.is_file() is not True):
+        data_Path.write_text('No data')
+        print(f'A new file has been created at: \n{data_Path}\nThis file holds your ToDo List')
+        time.sleep(3)
+
+    display()
+    #Initial User instructions. I picked this so that new users can get help,
+    #but old users won't always see a tutorial
+    print('\n\n\n')
+    print(f'Welcome, {username}! Enjoy the program and type "help" if needed')
+    
+    #Main program loop
+    while command != 'quit' or 'q':
+        command = get_Input()
+        handleInput(command)
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
